3282a5f65a791b4b8b68138e0b807182ab092a68
meron6 <khozameron5@gmail.com>
2024-02-27 07:31:35 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if the argument matches the pattern /School/
-def match_school(arg)
-  if arg =~ /School/
-    puts "School"
-  else
-    puts ""
-  end
-end
-
-# Get the argument from the command line
-arg = ARGV[0]
-
-# Call the matching method
-match_school(arg)
+puts ARGV[0].scan(/School/).join
